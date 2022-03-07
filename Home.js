7bb3830d9f70bef2b5b8dbe3978351c4572e0487
Poojamai08 <84305369+Poojamai08@users.noreply.github.com>
2022-03-07 11:31:18 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../Css/Home.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <nav className="nav-group">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/gallery">Gallery</Link>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <div className="btn">
+              <button className="signin_btn">
+                <Link to="/signin">SignIn</Link>
+              </button>
+              <button className="signup_btn">
+                <Link to="/signup">SignUp</Link>
+              </button>
+            </div>
+          </ul>
+        </nav>
+        <h1>React WebPage</h1>
+      </>
+    );
+  }
+}
+
+export default Home;
